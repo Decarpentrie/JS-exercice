a3f500d41cb0c4e742edc5b54181aa70225cf039
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-03-27 10:35:23 +0200

    ...

---
+++ array/js-exo.js
@@ -0,0 +1,11 @@
+var mois = [ "Janvier","Février", "Mars", "Avril", "mai", "juin", "Juillet", "aout", "Septembre", "Octobre", "Novembre", "Décembre"];
+
+mois.splice(7,1,"Août");
+
+
+
+
+console.log(mois[2])
+console.log(mois[4])
+console.log(mois[10])
+console.log(mois)
