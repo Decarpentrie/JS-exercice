b249562f1aa57cd99674b5f316528adc7ec0b031
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-03-27 11:27:44 +0200

    exercice 11

--- array/app.js
+++ array/app.js
@@ -1,10 +1,11 @@
 var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
-array.push("Courgette")
+array.push("Courgette");
 
 array.splice(5,0, "Citron");
 
-array.splice(1,1)
+array.splice(1,1);
 
+array.splice(2,0, "Poire");
 
-console.log(array)
+console.log(array);
