287bb0cb528f4f78fc4695c450395620b29e738c
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-03-27 11:22:34 +0200

    execercie 10 fait

--- array/app.js
+++ array/app.js
@@ -2,9 +2,9 @@ var array = ['Pomme','Cerise','Tomate','Citron', ['Orange','Poivron']];
 
 array.push("Courgette")
 
-array.splice(5,"Citron");
+array.splice(5,0, "Citron");
 
-
+array.splice(1,1)
 
 
 console.log(array)
